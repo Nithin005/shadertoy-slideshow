f23b355ac0600da3e2e1f1b81cf36ac3cd9c78ce
Nithin005 <mcleaannithin@gmail.com>
2020-10-01 22:24:07 +0530

    frame fix 3

--- app.js
+++ app.js
@@ -1,18 +1,28 @@
+let i=0;
+let data;
+let iframeElement = document.createElement('iframe');
+iframeElement.src="about:blank";
+iframeElement.allowFullscreen = true;
+iframeElement.width = '100%';
+iframeElement.height = '100%';
+iframeElement.className = 'frame';
+iframeElement.onload = ()=>{
+ // iframeElement.parentElement.style.background = 'none';
+}
+document.body.querySelector('div').appendChild(iframeElement);
 let xhttp = new XMLHttpRequest();
 xhttp.onreadystatechange = function() {
     if (this.readyState == 4 && this.status == 200) {
-      main(JSON.parse(this.responseText));
+      data = JSON.parse(this.responseText);
+      main();
     }
   };
-xhttp.open("GET", "data.json", true);
+xhttp.open("GET", "https://www.shadertoy.com/api/v1/shaders?key=NtnK4n&num=200&sort=popular", true);
 xhttp.send();
 
-function main(data){
-    let i=0;
-    let iframeElement = document.getElementById('frame');
-    setInterval(()=>{
+function main(){
         iframeElement.src='https://www.shadertoy.com/embed/'+data.Results[i]+'?gui=true&t=10&paused=false&muted=false';
         i++;
         console.log(iframeElement.src);
-    },10000);
+        setTimeout(()=>{main();},10000);
 }
